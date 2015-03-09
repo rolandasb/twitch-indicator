c922321235bb1b72792577e57d20b7bb9bc2bb7b
Rolandas Barysas <hello@rbrs.io>
2015-03-09 23:34:34 +0200

    refactored most of the application

--- run.py
+++ run.py
@@ -15,10 +15,242 @@ gtk.gdk.threads_init()
 import threading
 
 class Twitch:
+  def fetch_followed_channels(self, username):
+    """Fetch user followed channels and return a list with channel names."""
+    try:
+      self.followed_channels = []
+      
+      self.f = urllib.urlopen("https://api.twitch.tv/kraken/users/{0}/follows/channels?direction=DESC&limit=100&offset=0&sortby=created_at".format(username))
+      self.data = json.loads(self.f.read())
+
+      self.pages = (self.data['_total'] - 1) / 100
+      for page in range(0, self.pages + 1):
+        if page != 0:
+          self.f = urllib.urlopen("https://api.twitch.tv/kraken/users/{0}/follows/channels?direction=DESC&limit=100&offset={1}&sortby=created_at".format(username, (page * 100)))
+          self.data = json.loads(self.f.read())
+
+        for channel in self.data['follows']:
+          self.followed_channels.append(channel['channel']['display_name'])
+      
+      return self.followed_channels
+    except IOError:
+      return None
+
+  def fetch_live_streams(self, channels):
+    """Fetches live streams data from Twitch, and returns as list of dictionaries"""
+    try:
+      self.channels_count = len(channels)
+      self.live_streams = []
+      
+      self.pages = (self.channels_count - 1) / 75
+      for page in range(0, self.pages + 1):
+        self.offset = (page * 75) + 75
+        if (self.offset % 75 > 0):
+          self.offset = self.channels_count 
+        self.channels_offset = channels[(page * 75):self.offset]
+
+        self.f = urllib.urlopen("https://api.twitch.tv/kraken/streams?channel={0}".format(','.join(self.channels_offset)))
+        self.data = json.loads(self.f.read())
+
+        for stream in self.data['streams']:
+          # For some reason sometimes stream status and game is not present in
+          # twitch API.
+          try:
+            self.status = stream['channel']['status']
+          except KeyError:
+            self.status = ""
+          
+          st = {
+            'name': stream['channel']['display_name'],
+            'status': self.status,
+            'image': stream['channel']['logo'],
+            'url': "http://www.twitch.tv/%s" % stream['channel']['name']
+          }
+
+          self.live_streams.append(st)
+      return self.live_streams
+    except IOError:
+      return None
+
+class Indicator():
   def __init__(self):
-    self.twitchUsername = "xrbrs" # Your twitch username
+    # Setup applet icon depending on DE
+    self.desktop_env = os.environ.get('DESKTOP_SESSION')
+    if self.desktop_env == "pantheon":
+      self.applet_icon = "indicator_elementary.png"
+    else:
+      self.applet_icon = "indicator_ubuntu.png"
+
+    # Create applet
+    self.a = appindicator.Indicator(
+      'wallch_indicator',
+      os.path.dirname(os.path.abspath(__file__)) + "/icons/%s" % self.applet_icon,
+      appindicator.CATEGORY_APPLICATION_STATUS
+    )
+    self.a.set_status(appindicator.STATUS_ACTIVE)
+
+    # Setup menu
+    self.menu = gtk.Menu()
+    self.menuItems = [
+      gtk.MenuItem('Check now'),
+      gtk.SeparatorMenuItem(),
+      gtk.MenuItem('Settings'),
+      gtk.MenuItem('Quit')
+    ]
+
+    for i in self.menuItems:
+      self.menu.append(i)
+
+    self.a.set_menu(self.menu)
+
+    for i in self.menu.get_children():
+      i.show()
+
+    self.menuItems[0].connect('activate', self.refresh_streams_init)
+    self.menuItems[-2].connect('activate', self.settings_dialog)
+    self.menuItems[-1].connect('activate', self.quit)
+    
+    # Runtime variables
+    self.currentLiveStreams = []
+
+  def rebuild_menu(self):
+    self.menuItems = [
+      gtk.MenuItem('Check now'),
+      gtk.SeparatorMenuItem(),
+      gtk.MenuItem('Settings'),
+      gtk.MenuItem('Quit')
+    ]
+
+    self.menuItems[0].connect('activate', self.refresh_streams_init)
+    self.menuItems[-2].connect('activate', self.settings_dialog)
+    self.menuItems[-1].connect('activate', self.quit)
+
+  def refresh_menu(self):
+    """Rebuilds indicator menu."""
+    for i in self.menu.get_children():
+      self.menu.remove(i)
+
+    for i in self.menuItems:
+      self.menu.append(i)
+
+    for i in self.menu.get_children():
+      i.show()
+
+  def open_link(self, widget, url):
+    """Opens link in a default browser."""
+    webbrowser.open_new_tab(url)
+
+  def refresh_streams_init(self, widget):
+    """Initializes thread for stream refreshing."""
+    threading.Thread(target=self.refresh_streams, args=(widget)).start()
+
+  def settings_dialog(self, widget):
+    """Shows applet settings dialog."""
+    self.dialog = gtk.Dialog(
+      "Settings",
+      None,
+      gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+      (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
+       gtk.STOCK_OK, gtk.RESPONSE_ACCEPT)
+    )
+
+    self.box = gtk.HBox(False, 0)
+    self.box2 = gtk.HBox(False, 0)
+
+    self.label_username = gtk.Label("Twitch username")
+    self.input_username = gtk.Entry()
+
+    self.label_notifications = gtk.Label("Enable notifications")
+    self.checkbox_notifications = gtk.CheckButton()
+
+    self.box.pack_start(self.label_username, True, True, 5)
+    self.box.pack_start(self.input_username, True, True, 5)
+
+    self.box2.pack_start(self.label_notifications, True, True, 5)
+    self.box2.pack_start(self.checkbox_notifications, True, True, 5)
+    
+    self.dialog.vbox.pack_start(self.box)
+    self.dialog.vbox.pack_end(self.box2)
+
+    self.box.show()
+    self.box2.show()
+    self.label_username.show()
+    self.input_username.show()
+    self.label_notifications.show()
+    self.label_notifications.set_justify(gtk.JUSTIFY_LEFT)
+    self.checkbox_notifications.show()
+
+    self.dialog.run()
+    self.dialog.destroy()
+
+  def refresh_streams(self, items):
+    """Refreshes live streams list. Also pushes notifications when needed."""
+    self.rebuild_menu()
+    self.refresh_menu()
+
+    # Disable check now button in menu and update text.
+    self.menuItems[0].set_sensitive(False)
+    self.menuItems[0].set_label("Checking...")
+    self.refresh_menu()
+
+    # Create twitch instance and fetch followed channels.
+    self.tw = Twitch()
+    self.followed_channels = self.tw.fetch_followed_channels("xrbrs")
+
+    # If we can't retrieve channels, update menu accordingly.
+    if self.followed_channels == None:
+      self.menuItems.insert(2, gtk.MenuItem("Cannot retrieve channels"))
+      self.menuItems.insert(3, gtk.SeparatorMenuItem())
+      self.menuItems[2].set_sensitive(False)
+
+      # Re-enable "Check now" button
+      self.menuItems[0].set_sensitive(True)
+      self.menuItems[0].set_label("Check now")
+      self.refresh_menu()
+
+      # Stop further execution.
+      return
+
+    # Fetch live streams
+    self.live_streams = self.tw.fetch_live_streams(self.followed_channels)
+
+    # If we can't retrieve streams, update menu accordingly.
+    if self.live_streams == None:
+      self.menuItems.insert(2, gtk.MenuItem("Cannot retrieve live streams"))
+      self.menuItems.insert(3, gtk.SeparatorMenuItem())
+      self.menuItems[2].set_sensitive(False)
+
+      # Re-enable "Check now" button
+      self.menuItems[0].set_sensitive(True)
+      self.menuItems[0].set_label("Check now")
+      self.refresh_menu()
+
+      # Stop further execution.
+      return
+
+    # Update menu with live streams
+    self.streams_menu = gtk.Menu() 
+    self.menuItems.insert(2, gtk.MenuItem("Live channels ({0})".format(len(self.live_streams))))
+    self.menuItems.insert(3, gtk.SeparatorMenuItem())
+    self.menuItems[2].set_submenu(self.streams_menu)
+
+    for index, stream in enumerate(self.live_streams):
+      self.streams_menu.append(gtk.MenuItem(stream["name"]))
+      self.streams_menu.get_children()[index].connect('activate', self.open_link, stream["url"])
+    
+    for i in self.streams_menu.get_children():
+      i.show()
+    
+    # Push notifications of new streams
+    self.push_notifications(self.live_streams)
+
+    # Re-enable "Check now" button
+    self.menuItems[0].set_sensitive(True)
+    self.menuItems[0].set_label("Check now")
+    self.refresh_menu()
 
   def push_notifications(self, streams):
+    """Pushes notifications of every stream, passed as a list of dictionaries."""
     try:
       for stream in streams:
 
@@ -40,222 +272,12 @@ class Twitch:
     except IOError:
       return None
 
-
-  def fetch_followed_channels(self, username):
-    """Fetches user followed channels from Twitch.
-    
-    Args:
-      username: twitch account username.
-
-    Returns:
-      A list of followed channels. For example:
-
-      ["cohhcarnage", "itmejp", "ezekiel_iii"]
-    """
-    try:
-      self.f = urllib.urlopen("https://api.twitch.tv/kraken/users/%s/follows/channels?limit=100" % username)
-      self.data = json.loads(self.f.read())
-      self.followedChannels = []
-      for channel in self.data['follows']:
-        self.followedChannels.append(channel['channel']['display_name'])
-      return self.followedChannels
-    except IOError:
-      return None
-
-  def fetch_live_streams(self, channels):
-    """Fetches live streams from Twitch.
-
-    Args:
-      channels: twitch channels, passed as a list.
-
-    Returns:
-      A list of dictionaries, with live streams data. For example:
-
-      [
-        {
-          'name': 'cohhcarnage',
-          'status': '[DAY 500! Wooo!] H1Z1 Battle Royales',
-          'image': 'http://www.twitch.tv/channels/cohhcarnage.jpeg',
-          'url': 'http://www.twitch.tv/cohhcarnage'
-        },
-        {
-          'name': 'itmejp',
-          'status': 'Battle Royale w/ @itmeJP',
-          'image': 'http://www.twitch.tv/channels/itmejp.jpeg',
-          'url': 'http://www.twitch.tv/itmejp'
-        }
-      ]
-    """
-    try:
-      self.f = urllib.urlopen("https://api.twitch.tv/kraken/streams?channel=%s" % ','.join(channels))
-      self.data = json.loads(self.f.read())
-      self.liveStreams = []
-      for stream in self.data['streams']:
-        # For some reason sometimes stream status and game is not present in
-        # twitch API.
-        try:
-          self.status = stream['channel']['status']
-        except KeyError:
-          self.status = ""
-        
-        st = {
-          'name': stream['channel']['display_name'],
-          'status': self.status,
-          'image': stream['channel']['logo'],
-          'url': "http://www.twitch.tv/%s" % stream['channel']['name']
-        }
-
-        self.liveStreams.append(st)
-      return self.liveStreams
-    except IOError:
-      return None
-
-class Indicator():
-  def __init__(self):
-    # Setup applet icon depending on DE 
-    self.desktop_env = os.environ.get('DESKTOP_SESSION')
-    if self.desktop_env == "pantheon":
-      self.applet_icon = "indicator_elementary.png"
-    else:
-      self.applet_icon = "indicator_ubuntu.png"
-
-    # Create applet
-    self.a = appindicator.Indicator(
-      'wallch_indicator',
-      os.path.dirname(os.path.abspath(__file__)) + "/icons/%s" % self.applet_icon,
-      appindicator.CATEGORY_APPLICATION_STATUS
-    )
-    self.a.set_status(appindicator.STATUS_ACTIVE)
-
-    self.currentLiveStreams = []
-
-    self.menu = gtk.Menu()
-    self.streamsMenu = gtk.Menu()
-
-    self.streamsMenuDisabled = gtk.MenuItem('Loading streams...')
-    self.streamsMenuDisabled.set_sensitive(False)
-
-    self.streamsMenuItem = gtk.MenuItem("...")
-    self.streamsMenuItem.set_submenu(self.streamsMenu)
-
-    self.menuItems = [
-      gtk.MenuItem('Check now'),
-      gtk.SeparatorMenuItem(),
-      self.streamsMenuItem,
-      gtk.SeparatorMenuItem(),
-      gtk.MenuItem('Settings'),
-      gtk.MenuItem('Quit')
-    ]
-
-    self.menuStreamsItems = []
-
-    for i in self.menuItems:
-      self.menu.append(i)
-
-    self.a.set_menu(self.menu)
-
-    for i in self.menu.get_children():
-      i.show()
-
-    self.menuItems[0].connect('activate', self.refresh_streams_init)
-    self.menuItems[-2].connect('activate', self.settings_dialog)
-    self.menuItems[-1].connect('activate', self.quit)
-
-  def rebuild_menu(self):
-    for i in self.menu.get_children():
-      self.menu.remove(i)
-
-    for i in self.menuItems:
-      self.menu.append(i)
-
-    for i in self.menu.get_children():
-      i.show()
-
-  def open_link(self, widget, url):
-    webbrowser.open_new_tab(url)
-
-  def refresh_streams_init(self, widget):
-    threading.Thread(target=self.refresh_streams, args=(widget)).start()
-
-  def settings_dialog(self, widget):
-    self.dialog = gtk.Dialog(
-      "Settings",
-      None,
-      gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
-      (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
-       gtk.STOCK_OK, gtk.RESPONSE_ACCEPT)
-    )
-
-    self.box = gtk.HBox(False, 0)
-
-    self.label_username = gtk.Label("Twitch username")
-    self.input_username = gtk.Entry()
-
-    self.box.pack_start(self.label_username, True, True, 5)
-    self.box.pack_start(self.input_username, True, True, 5)
-    
-    self.dialog.vbox.pack_start(self.box)
-    
-    self.box.show()
-    self.label_username.show()
-    self.input_username.show()
-
-    self.dialog.run()
-    self.dialog.destroy()
-
-  def refresh_streams(self, items):
-    """Refreshes live streams.
-    
-    This function fetches followed channels, checks which ones are live, updates
-    menu accordingly and pushes notifications of new streams.
-
-    Args:
-      silent: should application hide notifications, passed as a boolean
-
-    """
-    self.menuItems[0].set_sensitive(False)
-    self.menuItems[0].set_label("Checking...")
-    self.menuItems[2] = self.streamsMenuDisabled
-    self.rebuild_menu()
-
-    self.tw = Twitch()
-    self.followedChannels = self.tw.fetch_followed_channels("xrbrs")
-
-    if self.followedChannels != None:
-      self.liveStreams = self.tw.fetch_live_streams(self.followedChannels)
-    else:
-      self.menuItems[0].set_sensitive(True)
-      self.menuItems[0].set_label("Check now")
-      self.menuItems[2].set_label("Cannot retrieve channels")
-      self.rebuild_menu()
-      return False
-
-    # Push notifications of new streams
-    self.tw.push_notifications(self.liveStreams)
-
-    # Update menu
-    self.streamsMenuItem.set_label("Live channels (%d)" % len(self.liveStreams))
-    for i in self.streamsMenu.get_children():
-      self.streamsMenu.remove(i)
-
-    self.menuStreamsItems = []
-    for index, stream in enumerate(self.liveStreams):
-      self.menuStreamsItems.append(gtk.MenuItem(stream["name"]))
-      self.menuStreamsItems[index].connect('activate', self.open_link, stream["url"])
-      self.streamsMenu.append(self.menuStreamsItems[index])
-
-    for i in self.streamsMenu.get_children():
-      i.show()
-
-    self.menuItems[2] = self.streamsMenuItem
-    self.rebuild_menu()
-    self.menuItems[0].set_label("Check now")
-    self.menuItems[0].set_sensitive(True)
-
   def main(self):
+    """Main indicator function."""
     gtk.main()
 
   def quit(self, item):
+    """Quits the applet."""
     gtk.main_quit()
 
 if __name__=="__main__":
@@ -263,4 +285,3 @@ if __name__=="__main__":
   gtk.gdk.threads_enter()
   gui.main()
   gtk.gdk.threads_leave()
-
